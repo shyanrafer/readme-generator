567a2a34add770a4fc3737a128cf19206f1d43bf
The Goose <ryanshaffer1324@gmail.com>
2024-06-26 19:36:32 -0400

    Changed files in gitignore and also added a couple questions via comments to files.

--- index.js
+++ index.js
@@ -2,14 +2,20 @@
 const fs = require('fs')
 const inquirer = require('inquirer')
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// where do i use inquirer?
+// TODO: Create an array of questions for user input - is this the function for the prompts?
+const questions = ["What's your Github username?", "What's your email?"];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  // will likely need to import generateMarkdown here
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
