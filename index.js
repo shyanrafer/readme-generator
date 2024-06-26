d8f96eb249dcdfdf36f0cd5c339704a507470884
The Goose <ryanshaffer1324@gmail.com>
2024-06-26 19:51:12 -0400

    Added questions to questions array and also re-added package-lock.json to gitignore.

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 // TODO: Include packages needed for this application
 const fs = require('fs')
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
 
 // where do i use inquirer?
 // TODO: Create an array of questions for user input - is this the function for the prompts?
-const questions = ["What's your Github username?", "What's your email?"];
+const questions = [
+  {
+    name: 'projectName',
+    message: 'What is the title of your project?'
+  },
+  {
+    name: 'description',
+    message: 'Write a brief description of your project.'
+  },
+  {
+    name: 'installInstructions',
+    message: 'How does one install your program?'
+  },
+  {
+    name: 'useGuide',
+    message: 'What is this project used for?'
+  },
+  {
+    name: 'contribution',
+    message: 'How does one contribute to this project?'
+  },
+  {
+    name: 'tests',
+    message: 'Include test instructions for your project.'
+  },
+  {
+    name: 'licenseType',
+    type: 'checkbox',
+    message: 'Choose a license.'
+  }
+]
 
 
 // TODO: Create a function to write README file
