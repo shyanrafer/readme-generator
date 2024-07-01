aff1a5d765af64f8b257006ad9a86fbffb172f60
The Goose <ryanshaffer1324@gmail.com>
2024-07-01 13:03:18 -0400

    Same test as #8 - trying to tie back to top to description instead.

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -71,7 +71,7 @@ Repo link: ${data.repoLink}
 ## Contact
 Best mode of contact: ${data.contact} <br/>
 Contact info: ${data.contactInfo} <br/>
-[Back to top](#projectName)
+[Back to top](#description)
 `;
 }
 // returns a string
